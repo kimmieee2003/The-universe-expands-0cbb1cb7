a96ba2e8b933c91b4c9a17f9d40f256b5dd66f74
Kimberly Hogeveen <2103405@talnet.nl>
2020-04-08 15:21:31 +0200

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,3 +1,5 @@
 /*CREATE DATABASE sterrenstelsel;*/
-USE sterrenstelsel;
-CREATE TABLE planeten (naam VARCHAR(20));
+/*USE sterrenstelsel;*/
+/*CREATE TABLE planeten (naam VARCHAR(20));*/
+insert into planeten (naam)  value ("Zon"), ("Mercurius"), ("Venus"), ("Aarde"), ("Mars");
+SELECT * FROM planeten;
